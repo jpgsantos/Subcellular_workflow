cd3537cf7154cd7125c7698f3ceb4bbcdd167bc1
danieltrpevski <daniel.trpevski@gmail.com>
2020-03-28 00:05:20 +0100

    Some minor updates

--- NEURON/single_section_run.py
+++ NEURON/single_section_run.py
@@ -8,6 +8,7 @@ Created on Thu Dec 15 17:48:41 2016
 import single_sec_cell as se
 import timing_experiment as pe
 import parameters as p
+import json
 
 cell = se.SingleSecCell()
 
@@ -17,4 +18,14 @@ ex.cell.somalist[0].DA_max_timing_model = 0
 ex.set_up_recording()
 ex.simulate()
 ex.plot_results()
+
+steady_states = []
+for sp in ex.species:
+    steady_states.append(sp.to_python()[-1])
+
+result = {'steady_states': steady_states}
+to_save = json.dumps(result)
+with open('steady_states.dat', 'w', encoding = 'utf-8') as f:
+    json.dump(to_save, f)
+    
 
