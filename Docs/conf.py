6082455be2191de454e975b077b9f6bd67bc31b8
jpgds <jpgs.12390@gmail.com>
2020-08-06 18:05:28 +0200

    website formating update

--- Docs/conf.py
+++ Docs/conf.py
@@ -55,7 +55,9 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-
+html_css_files = [
+    'css/my_theme.css'
+]
 
 # matlab source code
 
