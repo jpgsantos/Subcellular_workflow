7b8de0d6e0d294dcb7293a03d93513e8991b6e76
João Pedro Gomes dos Santos <jpgs_jpgs@hotmail.com>
2021-05-20 18:23:07 +0200

    Updated the names of the functions that generate the figures
    Distinguish between functions for plotting and generating data

--- Code for paper figures/Reproduce_figures.m
+++ Code for paper figures/Reproduce_figures.m
@@ -8,14 +8,34 @@ script_folder = convertCharsToStrings(matlab.desktop.editor.getActiveFilename);
 script_folder = strrep(script_folder,"\","/");
 script_folder = strrep(script_folder,mfilename + ".m","");
 
-f_Reproduce_new_data_fig3_CD(script_folder + "Reproduce paper figure 3/")
-f_Reproduce_fig3(script_folder + "Reproduce paper figure 3/")
-f_Reproduce_fig4(script_folder + "Reproduce paper figure 4/")
-f_Reproduce_fig5(script_folder + "Reproduce paper figure 5/")
-f_Reproduce_fig6(script_folder + "Reproduce paper figure 6/")
-f_Reproduce_fig7_BCDE(script_folder + "Reproduce paper figure 7/")
-f_Reproduce_sup_fig1(script_folder + "Reproduce paper supplementary figure 1/")
-f_Reproduce_sup_fig2(script_folder + "Reproduce paper supplementary figure 2/")
-f_Reproduce_sup_fig3(script_folder + "Reproduce paper supplementary figure 3/")
-f_Reproduce_sup_fig4(script_folder + "Reproduce paper supplementary figure 4/")
-f_Reproduce_sup_fig5(script_folder + "Reproduce paper supplementary figure 5/")
+%Runtimes measured using a intel i9-10980XE and MATLAB 2021a
+
+f_Reproduce_plot_fig3(script_folder + "Reproduce paper figure 3/")
+%Run time ~= 15 seconds
+
+f_Reproduce_plot_fig4(script_folder + "Reproduce paper figure 4/")
+%Run time ~= 7 seconds
+
+f_Reproduce_plot_fig5(script_folder + "Reproduce paper figure 5/")
+%Run time ~= 10 seconds
+
+f_Reproduce_plot_fig6(script_folder + "Reproduce paper figure 6/")
+%Run time ~= 34 seconds
+
+f_Reproduce_plot_fig7_BCDE(script_folder + "Reproduce paper figure 7/")
+%Run time ~= 8 seconds
+
+f_Reproduce_plot_sup_fig1(script_folder + "Reproduce paper supplementary figure 1/")
+%Run time ~= 10 seconds
+
+f_Reproduce_plot_sup_fig2(script_folder + "Reproduce paper supplementary figure 2/")
+%Run time ~= 31 seconds
+
+f_Reproduce_plot_sup_fig3(script_folder + "Reproduce paper supplementary figure 3/")
+%Run time ~= 15 seconds
+
+f_Reproduce_plot_sup_fig4(script_folder + "Reproduce paper supplementary figure 4/")
+%Run time ~= 18 seconds
+
+f_Reproduce_plot_sup_fig5(script_folder + "Reproduce paper supplementary figure 5/")3
+%Run time ~= 7 seconds
