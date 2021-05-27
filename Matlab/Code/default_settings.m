3d24da50036cd6c105e01b5bc0dbc970c5275f43
João Pedro Gomes dos Santos <jpgs_jpgs@hotmail.com>
2021-05-27 12:03:14 +0200

    Update default_settings.m

--- Matlab/Code/default_settings.m
+++ Matlab/Code/default_settings.m
@@ -82,6 +82,9 @@ stg.simtime = "second";
 % (sbioaccelerate)
 stg.sbioacc = true;
 
+% (Absolute tolerance step size for equilibration)
+stg.abstolstepsize_eq = [];
+
 % Max step size in the simulation (if empty matlab decides whats best)
 % (Maximum step)
 stg.maxstep = [10];
@@ -94,6 +97,10 @@ stg.maxstepeq = [2];
 % (Maximum step)
 stg.maxstepdetail = [2];
 
+% Default score when there is a simulation error, this is needed to keep
+% the optimizations working.
+% (error score)
+stg.errorscore = 10^5;
 %% Model
 
 % Number of parameters to optimize (Parameter number)
