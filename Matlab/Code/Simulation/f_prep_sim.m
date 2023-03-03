1aa7df3d09f72b4b492b6e9b15c8730ff5f5c4d2
João Pedro Gomes dos Santos <jpgs_jpgs@hotmail.com>
2023-03-03 16:01:46 +0100

    work 2023-03-01

--- Matlab/Code/Simulation/f_prep_sim.m
+++ Matlab/Code/Simulation/f_prep_sim.m
@@ -1,10 +1,13 @@
 function rst = f_prep_sim(parameters,stg,mmf)
-%F_PREP_SIM Prepare parameters for simulation
-%   This function prepares the parameters for a simulation by setting them
-%   to the default values defined in the SBTAB and then updating any
-%   parameters that are being tested. The parameters are also adjusted
-%   according to any thermodynamic constraints.
-
+%   F_PREP_SIM Prepare parameters for simulation This function prepares the
+%   parameters for a simulation by setting them to the default values
+%   defined in the SBTAB and then updating any parameters that are being
+%   tested. The parameters are also adjusted according to any thermodynamic
+%   constraints. 
+%   Inputs parameters: parameters
+%   stg: settings
+%   mmf: main model folders
+%   Outputs rst: results
 
 % Save variables that need to be mantained over multiple function calls
 persistent sbtab
@@ -20,7 +23,7 @@ end
 
 % Set the parameters that are going to be used for the simulation to the
 % default ones as definded in the SBTAB
-rt.par(:,1) = [sbtab.defpar{:,2}];
+sim_par(:,1) = [sbtab.defpar{:,2}];
 
 % Check if the parametrer needs to be set to the value relevant for Profile
 % Likelihood
@@ -29,37 +32,37 @@ if isfield(stg,"PLind") && isfield(stg,"PLval")
 end
 
 % Iterate over all the parameters of the model
-for n = 1:size(rt.par,1)
-    
+for n = 1:size(sim_par,1)
+
     % Check that a parameter should be changed from default
     if stg.partest(n) > 0
-        
+
         % Set the parameters are being tested
-        rt.par(n) = 10.^(parameters(stg.partest(n,1)));
+        sim_par(n) = 10.^(parameters(stg.partest(n,1)));
     end
-    
+
     if isfield(stg,'tci')
-        
+
         % Check that there are thermodynamic constraints to implement
         if ~isempty(stg.tci)
-            
+
             % Choose the parameters that need to be calculated with other
             % parameters due to thermodynamic constraints
             if ismember(n,stg.tci)
-                
+
                 % Check that a parameter should be changed from default
                 if stg.partest(n) > 0
-                    
+
                     % Iterate over the parameters that need to be mutiplied
                     % for calculating the parameter that depends on the
                     % thermodynamic constraints
                     for m = 1:size(stg.tcm,2)
-                        
+
                         % Check that the parameter that is going to be used
                         % to calculate the parameter dependent on
                         % thermodynamic constraintsis is not the default
                         if stg.partest(stg.tcm(n,m),1) > 0
-                            
+
                             % Check if the parametrer needs to be set to
                             % the value relevant for Profile Likelihood
                             if isfield(stg,"PLind")
@@ -70,31 +73,31 @@ for n = 1:size(rt.par,1)
                                         = stg.PLval;
                                 end
                             end
-                            
+
                             % Make the appropriate multiplications to get
                             % the thermodinamicly constrained parameter
-                            rt.par(n) = rt.par(n).*(10.^...
+                            sim_par(n) = sim_par(n).*(10.^...
                                 (parameters(stg.partest(...
                                 stg.tcm(n,m),1))));
                         else
-                            
+
                             % Make the appropriate multiplications to get
                             % the thermodinamicly constrained parameter
-                            rt.par(n) = rt.par(n).*...
+                            sim_par(n) = sim_par(n).*...
                                 (sbtab.defpar{stg.tcm(n,m),2});
                         end
                     end
-                    
+
                     % Iterate over the parameters that need to be divided
                     % for calculating the parameter that depends on the
                     % thermodynamic constraints
                     for m = 1:size(stg.tcd,2)
-                        
+
                         % Check that the parameter that is going to be used
                         % to calculate the parameter dependent on
                         % thermodynamic constraintsis is not the default
                         if stg.partest(stg.tcd(n,m),1) > 0
-                            
+
                             % Check if the parametrer needs to be set to
                             % the value relevant for Profile Likelihood
                             if isfield(stg,"PLind")
@@ -107,14 +110,14 @@ for n = 1:size(rt.par,1)
                             end
                             % Make the appropriate divisions to get the
                             % thermodinamicly constrained parameter
-                            rt.par(n) = rt.par(n)./(10.^...
+                            sim_par(n) = sim_par(n)./(10.^...
                                 (parameters(stg.partest(...
                                 stg.tcd(n,m),1))));
                         else
-                            
+
                             % Make the appropriate divisions to get the
                             % thermodinamicly constrained parameter
-                            rt.par(n) = rt.par(n)./...
+                            sim_par(n) = sim_par(n)./...
                                 (sbtab.defpar{stg.tcd(n,m),2});
                         end
                     end
@@ -125,29 +128,27 @@ for n = 1:size(rt.par,1)
 end
 
 %  Set the start amount for the species in the model to 0
-rt.ssa = zeros(size(sbtab.species,1),max(stg.exprun));
+ssa = zeros(size(sbtab.species,1),max(stg.exprun));
 
 % Initialize the results variable
 rst = [];
-rst.parameters = rt.par;
+rst.parameters = sim_par;
 % Iterate over all the experiments that are being run
 for n = stg.exprun
-    
+
     % Try catch used because iterations errors can happen unexectedly and
     % we want to be able to continue simulations
     try
-        
+
         % If the correct setting is chosen display messages to console
         if stg.simcsl
             disp("Running dataset number " + n +...
                 " of " + stg.exprun(end))
         end
-        
+
         % Check that this is not the first time the experiments are being
         % run and that the start values for the species are different from
         % the previous experiment
-
-
 
         if n ~= stg.exprun(1) && ...
                 min([sbtab.datasets(n).start_amount{:,2}] ==...
@@ -155,74 +156,61 @@ for n = stg.exprun
                 find(stg.exprun==n)-1))).start_amount{:,2}])...
                 && rst.simd{n-1} ~= 0
 
-
-%             disp("pass " + n) 
-            
             % Set the values of the start amounts to the values obtained
             % after the first equilibration
-            rt.ssa(:,n) =...
-                rt.ssa(:,stg.exprun(find(stg.exprun==n)-1));
+            ssa(:,n) =...
+                ssa(:,stg.exprun(find(stg.exprun==n)-1));
             if stg.simdetail
-                rt.ssa(:,n+2*stg.expn) =...
-                    rt.ssa(:,stg.exprun(find(stg.exprun==n)-1));
+                ssa(:,n+2*stg.expn) =...
+                    ssa(:,stg.exprun(find(stg.exprun==n)-1));
             end
         else
             if n ~= stg.exprun(1)
-%             disp("fail 1 " + min([sbtab.datasets(n).start_amount{:,2}] ==...
-%                 [sbtab.datasets(max(1,stg.exprun(...
-%                 find(stg.exprun==n)-1))).start_amount{:,2}]) + " "+ n)
 
-%            disp("fail 2 " + rst.simd{n-1} + " " + n)
             end
             % Iterate over the numbre of species that need a starting value
             % different than 0
             for j = 1:size(sbtab.datasets(n).start_amount,1)
-                
+
                 % Set the start amount of the species to the number defined
                 % in the sbtab for each experiment
-                rt.ssa(sbtab.datasets(n...
+                ssa(sbtab.datasets(n...
                     ).start_amount{j,3},n+stg.expn) =...
                     sbtab.datasets(n).start_amount{j,2};
             end
-            
+
             % Equilibrate the model
-            rst = f_sim(n+stg.expn,stg,rt,rst,mmf);
-%             disp("eq done " + n)
+            rst = f_sim(n+stg.expn,stg,sim_par,ssa,rst,mmf);
+
             for j = 1:size(sbtab.species,1)
-                
+
                 % Set the starting amount for species that after
                 % equilibrium have very low values to zero
-% n+stg.expn
-% j
-% rst.simd{n+stg.expn}
-% rst.simd{n+stg.expn}.Data
-% rst.simd{n+stg.expn}.Data(end,j)
 
                 if rst.simd{n+stg.expn}.Data(end,j) < 1.0e-15
-                    rt.ssa(j,n) = 0;
-                    
+                    ssa(j,n) = 0;
+
                     % Set the starting amount for the rest of the species
                 else
-                    rt.ssa(j,n) =...
+                    ssa(j,n) =...
                         rst.simd{n+stg.expn}.Data(end,j);
                     if stg.simdetail
-                        rt.ssa(j,n+2*stg.expn) =...
+                        ssa(j,n+2*stg.expn) =...
                             rst.simd{n+stg.expn}.Data(end,j);
                     end
                 end
             end
         end
-        
+
         % Simulate the model
-        rst = f_sim(n,stg,rt,rst,mmf);
-%         disp("sim done " + n)
+        rst = f_sim(n,stg,sim_par,ssa,rst,mmf);
         try
             if stg.simdetail
-                rst = f_sim(n+2*stg.expn,stg,rt,rst,mmf);
+                rst = f_sim(n+2*stg.expn,stg,sim_par,ssa,rst,mmf);
             end
         catch
         end
-        
+
         % Check If the times of the simultaion output and the simulation
         % data from SBTAB match, if they don't it means that the simulator
         % didn't had enough time to run the model (happens in some
@@ -235,12 +223,11 @@ for n = stg.exprun
             % not worked properly
             rst.simd{n} = 0;
         end
-%         disp("y " + n)
     catch ME
         disp(ME.identifier + " " + n)
-        
+
         % Set the simulation output to be 0, this is a non function value
-        % that the score function expects in simulations that did not
+        % th3at the score function expects in simulations that did not
         % worked properly
         rst.simd{n} = 0;
     end
--- Matlab/Code/Simulation/f_sim.m
+++ Matlab/Code/Simulation/f_sim.m
@@ -1,4 +1,13 @@
-function rst = f_sim(exp_n,stg,rt,rst,mmf)
+function rt = f_sim(experiment_number,settings,sim_par,ssa,rt,main_model_folders)
+% This function runs simulations using SimBiology models for a set of
+% experiments. It loads the appropriate models and compiles the code for
+% simulation run, then substitutes the start amounts of species and
+% parameter values based on the real-time results and runs the simulation.
+% The results of the simulation are saved in the output variable. The
+% function can be called multiple times for different experiments, and it
+% maintains the state of the loaded models between calls using persistent
+% variables. Inputs: experiment_number, settings, realttime_results,
+% results, main_model_folders Outputs: results
 
 % Save variables that need to be mantained over multiple function calls
 persistent model_run
@@ -8,79 +17,76 @@ persistent config_run
 % and compile the code for simulation run
 if isempty(model_run)
     
+    % Turn off warning messages
     warning('off','SimBiology:InvalidSpeciesInitAmtUnits')
     
     %Generate an empty array to be populated with the model suited for each
     %equilibration and experiment%
-    model_run = cell(1,stg.expn*2);
-    config_run = cell(1,stg.expn*2);
+    model_run = cell(1,settings.expn*2);
+    config_run = cell(1,settings.expn*2);
     
-    model_exp_default = mmf.model.data.model_exp.default;
-    model_exp_eq = mmf.model.data.model_exp.equilibration;
-    model_exp_detail = mmf.model.data.model_exp.detail;
+    % Set the file paths for the different models
+    model_exp_default = main_model_folders.model.data.model_exp.default;
+    model_exp_eq = main_model_folders.model.data.model_exp.equilibration;
+    model_exp_detail = main_model_folders.model.data.model_exp.detail;
     
     % Iterate over the experiments thar are being run
-    for n = stg.exprun
+    for n = settings.exprun
         
-        if stg.simdetail
-            % Load the models for equilibrium
-            load(model_exp_detail + n + ".mat",'model_exp','config_exp')
 
-            % Place the loaded models in the correct place in the array,
-            % models for equilibrium are set to be on the second half of
-            % the array
-            model_run{n+2*stg.expn} = model_exp;
-            config_run{n+2*stg.expn} = config_exp;
+        % Load the models for detailed simulation and place them in the
+        % correct place in the array
+        if settings.simdetail
+            load(model_exp_detail + n + ".mat",'model_exp','config_exp')
+            model_run{n+2*settings.expn} = model_exp;
+            config_run{n+2*settings.expn} = config_exp;
             
             % Compile the matlab code that is going to simulate the model
-            % using matlab built in function if the option is selected in
+            % using matlab built-in function if the option is selected in
             % settings
-            if stg.sbioacc
-                sbioaccelerate(model_run{n+2*stg.expn},config_run{n+2*stg.expn});
+            if settings.sbioacc
+                sbioaccelerate(model_run{n+2*settings.expn},config_run{n+2*settings.expn});
             end
         end
-        % Load the models for equilibrium
-        load(model_exp_eq + n + ".mat",'model_exp','config_exp')
 
-        % Place the loaded models in the correct place in the array, models
-        % for equilibrium are set to be on the second half of the array
-        model_run{n+stg.expn} = model_exp;
-        config_run{n+stg.expn} = config_exp;
+
+        % Load the models for equilibrium and place them in the correct
+        % place in the array
+        load(model_exp_eq + n + ".mat",'model_exp','config_exp')
+        model_run{n+settings.expn} = model_exp;
+        config_run{n+settings.expn} = config_exp;
         
         % Compile the matlab code that is going to simulate the model using
-        % matlab built in function if the option is selected in settings
-        if stg.sbioacc
-            sbioaccelerate(model_run{n+stg.expn},config_run{n+stg.expn});
+        % matlab built-in function if the option is selected in settings
+        if settings.sbioacc
+            sbioaccelerate(model_run{n+settings.expn},config_run{n+settings.expn});
         end
         
-        % Load the models for main run
+        % Load the models for main simulation and place them in the correct
+        % place in the array
         load(model_exp_default + n + ".mat",'model_exp','config_exp')
-        
-        % Place the loaded models in the correct place in the array, models
-        % for main run are set to be on the first half of the array
         model_run{n} = model_exp;
         config_run{n} = config_exp;
         
         % Compile the matlab code that is going to simulate the model using
         % matlab built in function if the option is selected in settings
-        if stg.sbioacc
+        if settings.sbioacc
             sbioaccelerate(model_run{n},config_run{n});
         end
     end
 end
 
-% accelerate(F)
-% tf = isaccelerated(model_run{exp_n});
-% disp(tf)
-
 % substitute the start amount of the species in the model with the correct
 % ones for  simulations
-set(model_run{exp_n}.species(1:size(rt.ssa(:,exp_n),1)),{'InitialAmount'},num2cell(rt.ssa(:,exp_n)))
+set(model_run{experiment_number}.species(1:size(ssa(:,experiment_number),1)),...
+    {'InitialAmount'},num2cell(ssa(:,experiment_number)));
 
 % Substitute the values of the parameters in the model for the correct one
 % for simultaions
-set(model_run{exp_n}.parameters(1:size(rt.par,1)),{'Value'},num2cell(rt.par))
+set(model_run{experiment_number}.parameters(1:size(sim_par,1)),...
+    {'Value'},num2cell(sim_par));
 
 %simulate the model using matlab built in function
-rst.simd{exp_n} = sbiosimulate(model_run{exp_n},config_run{exp_n});
+rt.simd{experiment_number} = sbiosimulate(model_run{experiment_number},...
+    config_run{experiment_number});
 end
