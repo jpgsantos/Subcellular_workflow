41eddf2cd0774381ac65f1c2aa16a70059dadcf3
João Pedro Gomes dos Santos <jpgs_jpgs@hotmail.com>
2023-04-04 19:42:41 +0200

    Improvements for the PLA
    PLA now as better parallel processing and the plots are improved to show multiple optimization methods.

--- Matlab/Code/Analysis/Profile Likelihood Analysis/f_PL_m.m
+++ Matlab/Code/Analysis/Profile Likelihood Analysis/f_PL_m.m
@@ -1,61 +1,199 @@
-function rst = f_PL_m(stg,mmf)
+function rst = f_PL_m(settings,model_folder)
 
 % Iterate over the parameters for which PL is going to be calculated
-for m = stg.pltest
-    
+for par_indx = settings.pltest
+
     % Find the index of the starting point for PL taking into consideration
     % the lower bound and upper bound of the parameter and number of points
     % to be calculated
-    [~,start_n{m}] = min(abs(stg.bestpa(m) -...
-        (stg.lb(m):(stg.ub(m) - stg.lb(m))/stg.plres:stg.ub(m))));
+    [~,PL_iter_start{par_indx}] = min(abs(settings.bestpa(par_indx) -...
+        (settings.lb(par_indx):(settings.ub(par_indx) - settings.lb(par_indx))/settings.plres:settings.ub(par_indx))));
 end
 
 % Iterate over the parameters for which PL is going to be calculated
-parfor m = stg.pltest
-    
-    % Calculate Profile Likelihood
-    [x{m},fval{m},simd{m}] = f_PL_s(m,start_n{m},stg,mmf);
+if settings.plsa
+a = 1:length(settings.pltest)*2;
+else
+a =[];
+end
+if settings.plfm
+b = length(settings.pltest)*2+1:length(settings.pltest)*4;
+else
+b = [];
+end
+
+parfor par_indx = [a,b]
+    [x{par_indx},fval{par_indx},simd{par_indx}] = f_PL_s(par_indx,PL_iter_start,settings,model_folder);
+end
+
+leng =length(settings.pltest);
+
+for par_indx = 1:leng
+if settings.plsa
+    x{1,par_indx}{1}(PL_iter_start{par_indx}-1:-1:1) = x{1,par_indx+leng}{1}(PL_iter_start{par_indx}-1:-1:1);
+    fval{1,par_indx}{1}(PL_iter_start{par_indx}-1:-1:1) = fval{1,par_indx+leng}{1}(PL_iter_start{par_indx}-1:-1:1);
+    simd{1,par_indx}{1}(PL_iter_start{par_indx}-1:-1:1) = simd{1,par_indx+leng}{1}(PL_iter_start{par_indx}-1:-1:1);
+end
+if settings.plfm
+    x{1,par_indx}{2}(PL_iter_start{par_indx}:settings.plres+1) = x{1,par_indx+leng*2}{2}(PL_iter_start{par_indx}:settings.plres+1);
+    x{1,par_indx}{2}(PL_iter_start{par_indx}-1:-1:1) = x{1,par_indx+leng*2}{2}(PL_iter_start{par_indx}-1:-1:1);
+    fval{1,par_indx}{2}(PL_iter_start{par_indx}:settings.plres+1) = fval{1,par_indx+leng*2}{2}(PL_iter_start{par_indx}:settings.plres+1);
+    fval{1,par_indx}{2}(PL_iter_start{par_indx}-1:-1:1) = fval{1,par_indx+leng*3}{2}(PL_iter_start{par_indx}-1:-1:1);
+    simd{1,par_indx}{2}(PL_iter_start{par_indx}:settings.plres+1) = simd{1,par_indx+leng*2}{2}(PL_iter_start{par_indx}:settings.plres+1);
+    simd{1,par_indx}{2}(PL_iter_start{par_indx}-1:-1:1) = simd{1,par_indx+leng*2}{2}(PL_iter_start{par_indx}-1:-1:1);
+end
 end
 
 % Assign the values of x and fval to the correct struct entries, this needs
 % to be done because struct assignemnts don't work inside parfor loop
-for n = stg.pltest
-    if stg.plsa
+for n = settings.pltest
+    if settings.plsa
         rst.sa.xt(n) = x{n}(1);
         rst.sa.fvalt(n) = fval{n}(1);
         rst.sa.simdt(n) = simd{n}(1);
     end
-    if stg.plfm
+    if settings.plfm
         rst.fm.xt(n) = x{n}(2);
         rst.fm.fvalt(n) = fval{n}(2);
         rst.fm.simdt(n) = simd{n}(2);
     end
 end
-
-% for j = stg.exprun
-% exprun = stg.exprun;
-% % useLog = stg.useLog;
-% stg.exprun = j;
-% % stg.useLog = 5;
-% parfor m = stg.pltest
-%     % Calculate Profile Likelihood
-%     [x{m},fval{m},simd{m}] = f_PL_s(m,start_n{m},stg,mmf);
-% end
-% % Assign the values of x and fval to the correct struct entries, this needs
-% % to be done because struct assignemnts don't work inside parfor loop
-% for n = stg.pltest
-%     if stg.plsa
-%         rst.sa.x{j}(n) = x{n}(1);
-%         rst.sa.fval{j}(n) = fval{n}(1);
-%         rst.sa.simd{j}(n) = simd{n}(1);
-%     end
-%     if stg.plfm
-%         rst.fm.x{j}(n) = x{n}(2);
-%         rst.fm.fval{j}(n) = fval{n}(2);
-%         rst.fm.simd{j}(n) = simd{n}(2);
-%     end
-% end
-% stg.exprun = exprun;  
-% % stg.useLog = useLog;
-% end
 end
+
+function [x,fval,simd] = f_PL_s(par_indx,PL_iter_start,settings,model_folders)
+par_indx_helper = par_indx;
+
+if par_indx_helper <= length(settings.pltest)
+    PL_iter_start = PL_iter_start{par_indx};
+    PL_iter(:) = PL_iter_start:settings.plres+1;
+elseif par_indx_helper <= length(settings.pltest)*2
+    par_indx = par_indx-length(settings.pltest);
+    PL_iter_start = PL_iter_start{par_indx};
+    PL_iter(:) = PL_iter_start-1:-1:1;
+elseif par_indx_helper <= length(settings.pltest)*3
+    par_indx = par_indx-length(settings.pltest)*2;
+    PL_iter_start = PL_iter_start{par_indx};
+    PL_iter(:) = PL_iter_start:settings.plres+1;
+else
+    par_indx = par_indx-length(settings.pltest)*3;
+    PL_iter_start = PL_iter_start{par_indx};
+    PL_iter(:) = PL_iter_start-1:-1:1;
+end
+
+%Set the parameter for which we are going to perform the profile Likelihood
+%calculations to the correct one
+settings.PLind = par_indx;
+
+temp_array = settings.bestpa;
+temp_array(par_indx) = [];
+temp_lb = settings.lb;
+temp_lb(par_indx) = [];
+temp_up = settings.ub;
+temp_up(par_indx) = [];
+
+% Initialize variables
+if settings.plsa
+    x{1} = cell(settings.plres+1,1);
+    fval{1} = zeros(settings.plres+1,1);
+    simd{1}= cell(settings.plres+1,1);
+    % Set the starting point of PL to the best solution found so far
+    x{1}{PL_iter_start} = temp_array;
+end
+if settings.plfm
+    x{2} = cell(settings.plres+1,1);
+    fval{2} = zeros(settings.plres+1,1);
+    simd{2}= cell(settings.plres+1,1);
+    % Set the starting point of PL to the best solution found so far
+    x{2}{PL_iter_start} = temp_array;
+end
+
+
+
+%  Iterate over the values for which PL is going to be calculated starting
+%  at the best value going up until the upper bound is reached, going back
+%  to the best and going down until the lower bound is reached
+for PL_iter_current = PL_iter
+
+    % Set the value of the parameter that is being worked on
+    settings.PLval = settings.lb(par_indx)+(settings.ub(par_indx)-settings.lb(par_indx))/...
+        settings.plres*PL_iter_current-(settings.ub(par_indx)-settings.lb(par_indx))/settings.plres;
+
+    % Set offset so each optimization allways starts from with the
+    % appropriate parameter array
+    if PL_iter_current == PL_iter_start
+        offset = 0;
+    elseif PL_iter_current > PL_iter_start
+        offset = -1;
+    elseif PL_iter_current < PL_iter_start
+        offset = 1;
+    end
+
+    if par_indx_helper <= length(settings.pltest)*2
+        % Run simulated annealing if chosen in settings
+        if settings.plsa
+            [x{1}{PL_iter_current},fval{1}(PL_iter_current),simd{1}{PL_iter_current}] =...
+                sim_a(PL_iter_current,PL_iter_start,x{1}{PL_iter_current+offset},temp_lb,temp_up,settings,model_folders);
+            name = "simulated annealing";
+            fval_show = fval{1}(PL_iter_current);
+        end
+    else
+        % Run fmincon if chosen in settings
+        if settings.plfm
+            [x{2}{PL_iter_current},fval{2}(PL_iter_current),simd{2}{PL_iter_current}] =...
+                fmin_con(PL_iter_current,PL_iter_start,x{2}{PL_iter_current+offset},temp_lb,temp_up,settings,model_folders);
+            name = "fmincon";
+            fval_show = fval{2}(PL_iter_current);
+        end
+    end
+    % Display console messages if chosen in settings
+    if settings.placsl
+        disp(name + "m: " + settings.PLind + "  n: " + PL_iter_current +...
+            "  PLval: " + settings.PLval + "  fval: " + fval_show)
+    end
+
+end
+end
+
+function [x,fval,simd] =...
+    sim_a(PL_iter_current,PL_iter_start,x,temp_lb,temp_up,settings,model_folders)
+% Get the optimization options from settings
+
+if PL_iter_current == PL_iter_start
+    options = optimoptions(@simulannealbnd,'Display','off', ...
+        'InitialTemperature',...
+        ones(1,settings.parnum-1)*1,'MaxTime',7.5,'ReannealInterval',35);
+else
+    options = settings.plsao;
+
+    % number of parameters changed from defenition in settings
+    options.InitialTemperature = ones(1,settings.parnum-1)*1;
+end
+% Optimize the model
+[x,fval] = simulannealbnd(@(x)f_sim_score(x,settings,model_folders),...
+    x,temp_lb,temp_up,options);
+
+[~,rst,~] = f_sim_score(x,settings,model_folders);
+simd = rst.simd{1,1};
+end
+
+function [x,fval,simd] =...
+    fmin_con(PL_iter_current,PL_iter_start,x,temp_lb,temp_up,settings,model_folders)
+if PL_iter_current == PL_iter_start
+    options = optimoptions('fmincon','Display','off',...
+        'Algorithm','interior-point',...
+        'MaxIterations',20,'OptimalityTolerance',0,...
+        'StepTolerance',1e-6,'FiniteDifferenceType','central');
+else
+    % Get the optimization options from settings
+    options = settings.plfmo;
+end
+
+% Optimize the model
+[x,fval] = fmincon(@(x)f_sim_score(x,settings,model_folders),...
+    x,[],[],[],[],temp_lb,temp_up,[],options);
+
+[~,rst,~] = f_sim_score(x,settings,model_folders);
+simd = rst.simd{1,1};
+
+end
+
--- Matlab/Code/Plots/f_plot_PL.m
+++ Matlab/Code/Plots/f_plot_PL.m
@@ -79,26 +79,26 @@ for m = stg.pltest
     % proper subploting position
     [fig_n,layout,plots] = f_get_subplot(size(stg.pltest,2),plot_n,...
         fig_n,"Parameter Profile Likelihood",layout,plots);
-    
+
     nexttile(layout);
 
-if mod(plot_n,12) == 1
+    if mod(plot_n,12) == 1
 
-% Add a legend to the figure
-Lgnd = legend('show','Orientation','Horizontal','fontsize',Legend_FontSize);
-Lgnd.Layout.Tile = 'North';
-legend boxoff
-xlabel(layout,"log_{10} \theta",...
-    'FontSize', Axis_FontSize,'Fontweight',Axis_Fontweight)
-ylabel(layout,"-2 PL",...
-    'FontSize', Axis_FontSize,'Fontweight',Axis_Fontweight)
-title(layout,"Parameter Profile Likelihood",...
-    'FontSize', Major_title_FontSize,'Fontweight',Major_title_Fontweight)
+        % Add a legend to the figure
+        Lgnd = legend('show','Orientation','Horizontal','fontsize',Legend_FontSize);
+        Lgnd.Layout.Tile = 'North';
+        legend boxoff
+        xlabel(layout,"log_{10} \theta",...
+            'FontSize', Axis_FontSize,'Fontweight',Axis_Fontweight)
+        ylabel(layout,"-2 PL",...
+            'FontSize', Axis_FontSize,'Fontweight',Axis_Fontweight)
+        title(layout,"Parameter Profile Likelihood",...
+            'FontSize', Major_title_FontSize,'Fontweight',Major_title_Fontweight)
 
-end
+    end
 
     plot_n = plot_n +1;
-    
+
     hold on
 
     % Check if there are results from PL function
@@ -107,7 +107,7 @@ end
         if isfield(rst.PLA,'sa')
             plot(stg.lb(m):(stg.ub(m)-stg.lb(m))/stg.plres:stg.ub(m),...
                 [rst.PLA.sa.fvalt{m}],'DisplayName','Total score sa',...
-                'LineWidth',1.5,'color','k')
+                'LineWidth',1.5,'color',[0, 0, 1, 0.5])
             minfval = min(rst.PLA.sa.fvalt{m});
         end
 
@@ -115,7 +115,7 @@ end
         if isfield(rst.PLA,'fm')
             plot(stg.lb(m):(stg.ub(m)-stg.lb(m))/stg.plres:stg.ub(m),...
                 [rst.PLA.fm.fvalt{m}],'DisplayName','Total score fmincon',...
-                'LineWidth',1.5,'color','k')
+                'LineWidth',1.5,'color',[1, 0, 0, 0.5])
             minfval = min(rst.PLA.fm.fvalt{m});
         end
     end
